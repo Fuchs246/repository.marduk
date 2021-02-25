9e8626722f8330143ac17f5de3707f274e758889
marduklev <36476590+marduklev@users.noreply.github.com>
2021-02-25 15:20:19 +0100

    fix syntax error

--- misc/create_musicvideoNFO_subroutine.bat
+++ misc/create_musicvideoNFO_subroutine.bat
@@ -8,7 +8,6 @@ ECHO -------------------------------------------------------------------------
 ECHO.
 PAUSE > NUL
 
-setlocal
 for /r %%A in (*.mkv *.avi *.mp4) do call :subroutine_createnfo "%%~nA"
 ECHO.
 ECHO 	Creatin is done
@@ -25,6 +24,7 @@ echo.
 echo 		%artist% - %title%.nfo
 echo 		Is Created
 echo.
+
 for /r %%a in (*.mkv *.avi *.mp4) do (
 (
 echo ^<?xml version="1.0" encoding="UTF-8" standalone="yes"?^>
@@ -43,14 +43,14 @@ echo ^	^<status^>^</status^>
 echo ^	^<studio^>^</studio^>
 echo ^	^<art^>
 for /r %%a in (*-thumb.jpg) do echo ^		^<thumb^>%~dp0%artist% - %title%\%artist% - %title%-thumb.jpg^</thumb^>
-for /r %%a in (*-fanart.jpg) do echo ^		^<fanart^>%~dp0%artist% - %title%\%artist% - %title%^-fanart.jpg^</fanart^>
-for /r %%a in (*-poster.jpg) do echo ^		^<poster^>%~dp0%artist% - %title%\%artist% - %title%^-poster.jpg^</poster^>
-for /r %%a in (*-artistthumb.jpg) do echo ^		^<artistthumb^>%~dp0%artist% - %title%\%artist% - %title%^-poster.jpg^</artistthumb^>
-for /r %%a in (*-banner.jpg) do echo ^		^<banner^>%~dp0%artist% - %title%\%artist% - %title%^-banner.jpg^</banner^>
-for /r %%a in (*-clearlogo.png) do echo ^		^<clearlogo^>%~dp0%artist% - %title%\%artist% - %title%^-clearlogo.png^"</clearlogo^>
-for /r %%a in (*-discart.jpg) do echo ^		^<discart^>%~dp0%artist% - %title%\%artist% - %title%^-discart.jpg^</discart^>
-for /r %%a in (*-discart.png) do echo ^		^<discart^>%~dp0%artist% - %title%\%artist% - %title%^-discart.png^</discart^>
-for /r %%a in (*-landscape.jpg) do echo ^		^<landscape^>%~dp0%artist% - %title%\%artist% - %title%^-landscape.jpg^</landscape^>
+for /r %%a in (*-fanart.jpg) do echo ^		^<fanart^>%~dp0%artist% - %title%\%artist% - %title%-fanart.jpg^</fanart^>
+for /r %%a in (*-poster.jpg) do echo ^		^<poster^>%~dp0%artist% - %title%\%artist% - %title%-poster.jpg^</poster^>
+for /r %%a in (*-artistthumb.jpg) do echo ^		^<artistthumb^>%~dp0%artist% - %title%\%artist% - %title%-poster.jpg^</artistthumb^>
+for /r %%a in (*-banner.jpg) do echo ^		^<banner^>%~dp0%artist% - %title%\%artist% - %title%-banner.jpg^</banner^>
+for /r %%a in (*-clearlogo.png) do echo ^		^<clearlogo^>%~dp0%artist% - %title%\%artist% - %title%-clearlogo.png^</clearlogo^>
+for /r %%a in (*-discart.jpg) do echo ^		^<discart^>%~dp0%artist% - %title%\%artist% - %title%-discart.jpg^</discart^>
+for /r %%a in (*-discart.png) do echo ^		^<discart^>%~dp0%artist% - %title%\%artist% - %title%-discart.png^</discart^>
+for /r %%a in (*-landscape.jpg) do echo ^		^<landscape^>%~dp0%artist% - %title%\%artist% - %title%-landscape.jpg^</landscape^>
 echo ^	^</art^>
 echo ^</musicvideo^>
 )>"%~dp0%artist% - %title%\%artist% - %title%.nfo"
